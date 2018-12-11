f590dec8d9435be121ffb326a7ffcf97d9422a78
Brian <blavassaur@gmail.com>
2018-12-11 16:46:40 +0900

    added user model

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express     = require("express"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    bodyParser  = require("body-parser"),
+    User        = require("./models/user"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
+
 mongoose.connect("mongodb://localhost/auth_demo_app", { useNewUrlParser: true });
 
 var app = express();
+
+
+app.use(require("express-session")({
+    secret: "idk my mans",
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.set('view engine', 'ejs');
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
